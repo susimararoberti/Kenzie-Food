c55afd2b2bbe64d383111e13c1a21e08658df401
Clara-Carvalho <99266901+Clara-Carvalho@users.noreply.github.com>
2022-05-17 17:00:43 -0300

    feat: Campo de Busca Criado

--- src/Js/Controller/indexControler.js
+++ src/Js/Controller/indexControler.js
@@ -14,3 +14,41 @@ async function mostrarProdutos(){
 }
 
 mostrarProdutos()
+
+const produtos = await Produtos.mostrarProdutosPublicos()
+
+const buscar = document.getElementById("buscar")
+
+const produtosPrivados = await Produtos.mostrarProdutosPrivados()
+
+console.log(produtos)
+        
+function buscarProdutos(){
+    vitrine.innerHTML = "" 
+    const dataBusca = []
+
+    if(localStorage.getItem("Token" == "")){
+        for(let i = 0; i < produtos.length; i++){
+            if (produtos[i].nome.toLowerCase().includes(buscar.value.toLowerCase()) == true || produtos[i].categoria.toLowerCase().includes(buscar.value.toLowerCase) == true){
+                dataBusca.push(produtos[i])
+            }
+        }
+        }else{
+            
+            for(let i = 0; i < produtos.length; i++){
+                if (produtos[i].nome.toLowerCase().includes(buscar.value.toLowerCase()) == true || produtos[i].categoria.toLowerCase().includes(buscar.value.toLowerCase) == true){
+                    console.log("entrou no if")
+                    dataBusca.push(produtos[i])
+                }
+            }for(let i = 0; i < produtosPrivados.length; i++){
+                if (produtosPrivados[i].nome.toLowerCase().includes(buscar.value.toLowerCase()) == true || produtosPrivados[i].categoria.toLowerCase().includes(buscar.value.toLowerCase) == true){
+                    dataBusca.push(produtos[i])
+                }
+            }
+
+        }
+        ListarProdutos.listarPosts(dataBusca)
+        buscar.addEventListener("keypress", buscarProdutos)
+}
+
+buscarProdutos()
