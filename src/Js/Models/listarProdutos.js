b1325c855ae6e2ada4e6e53668d2873a4317dc7e
SusimaraRoberti <susimararoberti@outlook.com>
2022-05-19 10:32:07 -0300

    feat: realizado ajustes do html Home e CSS da Home

--- src/Js/Models/listarProdutos.js
+++ src/Js/Models/listarProdutos.js
@@ -1,46 +1,55 @@
+import Carrinho from "../Database/Api_Carrinho.js";
+import MostrarCarrinhoLogado from "../Controller/indexControler.js";
+
 class ListarProdutos {
-    static async listarPosts(data){
-let arr = []
-console.log(data[0].id)
-        for (let i = 0; i < data.length; i++) {
-            arr.push(data[i])
-            const vitrine = document.getElementById("vitrine")
-            const gerarCard = document.createElement("div")
-            gerarCard.classList = "cardsClass"
-            const img = document.createElement("img")
-            img.classList = "imgDaVitrine"
-            const p = document.createElement("p")
-            p.classList = "paragrafos"
-            const titulo = document.createElement("h2")
-            const button = document.createElement("button")
-            const preco = document.createElement("p")
-            const divInferiorBtnPreco = document.createElement("div")
-            const btnConpra = document.createElement("button")
-            btnConpra.classList = "btnDeConpra"
-            divInferiorBtnPreco.classList = "divInferiorBtnPreco"
+  static async listarProdutos(data) {
+    let arr = [];
+    // console.log(data[0].id)
+    for (let i = 0; i < data.length; i++) {
+      // console.log(data[i].id)
+      arr.push(data[i]);
+      const vitrine = document.getElementById("vitrine");
+      const gerarCard = document.createElement("div");
+      const img = document.createElement("img");
+      const titulo = document.createElement("h2");
+      const p = document.createElement("p");
+      const span = document.createElement("span");
+      const divInferior = document.createElement("div");
+      const preco = document.createElement("h3");
+      const btnConpra = document.createElement("button");
 
-            img.src = data[i].imagem
-            p.innerText = data[i].descricao
-            button.innerText = data[i].categoria
-            titulo.innerText = data[i].nome
-            preco.innerText = "R$ " + data[i].preco
-            btnConpra.innerHTML = "&#128722;"
-            
-            vitrine.appendChild(gerarCard)
-            gerarCard.appendChild(img)
-            gerarCard.appendChild(titulo)
-            gerarCard.appendChild(p)
-            gerarCard.appendChild(button)
-            gerarCard.appendChild(divInferiorBtnPreco)
+      gerarCard.classList = "cards";
+      img.classList = "cards--img";
+      divInferior.classList = "card--footer";
 
-            divInferiorBtnPreco.appendChild(preco)
-            divInferiorBtnPreco.appendChild(btnConpra)
+      btnConpra.classList = "footer--add";
+      btnConpra.id = data[i].id;
+      btnConpra.addEventListener("click", async function () {
+        await Carrinho.adicionarAoCarrinho({
+          product_id: btnConpra.id,
+        });
+        window.location.reload();
+        // MostrarCarrinhoLogado.mostrarCarrinhoLogado()
+      });
 
+      img.src = data[i].imagem;
+      titulo.innerText = data[i].nome;
+      p.innerText = data[i].descricao;
+      span.innerText = data[i].categoria;
+      preco.innerText = "R$ " + data[i].preco;
+      btnConpra.innerHTML = "&#128722;";
 
-        }
-    return arr
-    }   
-    
+      gerarCard.appendChild(img);
+      gerarCard.appendChild(titulo);
+      gerarCard.appendChild(p);
+      gerarCard.appendChild(span);
+      divInferior.appendChild(preco);
+      divInferior.appendChild(btnConpra);
+      gerarCard.appendChild(divInferior);
+      vitrine.appendChild(gerarCard);
+    }
+    return arr;
+  }
 }
 
-export default ListarProdutos
+export default ListarProdutos;
